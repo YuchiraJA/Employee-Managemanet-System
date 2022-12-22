f9c205dfbaaf46541101a5d05b484869d97bf5ae
Yuchira Athukorala <yuchira.athukorala@gmail.com>
2022-12-22 23:35:27 +0530

    upload fixed project

---
+++ Fullstack UI/src/app/models/employees.model.ts
@@ -0,0 +1,10 @@
+export interface Employee {
+    id: string;
+    firstname: string;
+    lastname:string;
+    address: string;
+    email: string;
+    phone: string;
+    birthday: string;
+    department: string;
+}
